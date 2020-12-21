d6621df802f6a5f97b49844c4137a6375bf75690
nabil <nabilmebrouk40@gmail.com>
2020-12-21 16:18:12 +0100

    Initial Commit

---
+++ prova_git/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  prova_git
+//
+//  Created by Nabil Mebrouk on 21/12/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
