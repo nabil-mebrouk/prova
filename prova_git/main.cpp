8e81e71823b20f0858de5b48884ce4f47f80f1d6
nabil <nabilmebrouk40@gmail.com>
2020-12-21 16:22:29 +0100

    ok

--- prova_git/main.cpp
+++ prova_git/main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout<<"prova 2";
+    cout << "Hello, World!\n";
     return 0;
 }
